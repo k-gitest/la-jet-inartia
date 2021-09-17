10e720425a42a5a0ad4c2f66afe7e32cc46c3438
k-gitest <adobe_blow@yahoo.co.jp>
2021-09-17 10:46:00 +0000

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //アプリケーション全体設定でもHTTPSを使うように設定
+        \URL::forceScheme('https');
+    }
+}
